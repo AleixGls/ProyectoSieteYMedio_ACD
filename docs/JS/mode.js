4d7eca8587cbfb51a194bb998d37b69750ed8a60
Dpcrack22 <davidperera2006@gmail.com>
2025-01-10 17:13:36 +0100

    wp2 10 ene

--- docs/JS/mode.js
+++ docs/JS/mode.js
@@ -10,7 +10,7 @@ buttonMode.addEventListener('click', () => {
         navbar.classList.remove('navbar-osc');
         navlinks.classList.remove('nav-links-osc');
         footer.classList.remove('footer-osc');
-        buttonMode.textContent = "Modo Oscuro"; 
+        buttonMode.innerHTML = "🌙"; 
 
         body.classList.add('body');
         navbar.classList.add('navbar');
@@ -26,6 +26,37 @@ buttonMode.addEventListener('click', () => {
         navbar.classList.add('navbar-osc');
         footer.classList.add('footer-osc');
         navlinks.classList.add('nav-links-osc');
-        buttonMode.textContent = "Modo Claro";
+        buttonMode.innerHTML = "☀️";
+    }
+});
+
+
+
+const buttonModee = document.getElementById('Modee');
+
+
+buttonModee.addEventListener('click', () => {
+    if (body.classList.contains('body-osc')) {
+        body.classList.remove('body-osc');
+        navbar.classList.remove('navbar-osc');
+        navlinks.classList.remove('nav-links-osc');
+        footer.classList.remove('footer-osc');
+        buttonModee.innerHTML = "🌙"; 
+
+        body.classList.add('body');
+        navbar.classList.add('navbar');
+        footer.classList.add('footer');
+        navlinks.classList.add('nav-links');
+    } else {
+        body.classList.remove('body');
+        navbar.classList.remove('navbar');
+        footer.classList.remove('footer');
+        navlinks.classList.remove('nav-links');
+
+        body.classList.add('body-osc');
+        navbar.classList.add('navbar-osc');
+        footer.classList.add('footer-osc');
+        navlinks.classList.add('nav-links-osc');
+        buttonModee.innerHTML = "☀️";
     }
 });
