c73db52c9c7d82542cd5dfb3675cf8fcabf88d98
AleixGls <aleixgls@gmail.com>
2025-01-22 04:25:53 +0100

    iniciar.py ordenado

--- Iniciar.py
+++ Iniciar.py
@@ -3,8 +3,7 @@ import SC.Datos
 import SC.Bbdd
 
 
-if __name__ == "__main__":
-    SC.Bbdd.player_database()
+if __name__ == "__main__": 
     SC.Bbdd.card_database()
-    SC.Datos.context_game["cards_deck"] = SC.Datos.cartas_es
+    SC.Bbdd.player_database()
     SC.Menu.main_menu()
